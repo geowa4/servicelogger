acd71a5753116654d6a25d2c0e1ec9c81eac9019
George Adams <geadams@redhat.com>
2024-01-19 15:02:01 -0500

    Create Internal Service Log TUI
    Creates `servicelogger internal` for collecting the data to send as an internal service log.

---
+++ pkg/internalservicelog/model.go
@@ -0,0 +1,110 @@
+package internalservicelog
+
+import (
+	"errors"
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type model struct {
+	windowWidth  int
+	windowHeight int
+	inputForm    *huh.Form
+	confirmForm  *huh.Form
+
+	slSummary    string
+	slBody       string
+	confirmation bool
+}
+
+func initialModel() *model {
+	m := &model{}
+	m.createNewForms()
+	return m
+}
+
+func (m *model) createNewForms() {
+	m.inputForm = huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().Value(&m.slSummary).Title("Brief Summary").Validate(func(s string) error {
+				if len(s) == 0 {
+					return errors.New("summary cannot be empty")
+				}
+				return nil
+			}),
+			huh.NewText().Value(&m.slBody).Title("Body").CharLimit(1000),
+		).Title("Internal Service Log (Markdown Supported)"),
+	)
+	m.confirmForm = huh.NewForm(
+		huh.NewGroup(
+			huh.NewConfirm().
+				Value(&m.confirmation).
+				Title("Send this service log?").
+				Negative("Edit").
+				Affirmative("Send"),
+		),
+	)
+}
+
+func (m *model) Markdown() string {
+	return fmt.Sprintf("# %s\n\n%s\n", m.slSummary, m.slBody)
+}
+
+func (m *model) Init() tea.Cmd {
+	return tea.Batch(m.inputForm.Init(), m.confirmForm.Init())
+}
+
+type FailedConfirmationMsg struct{}
+
+func FailedConfirmation() tea.Msg {
+	return FailedConfirmationMsg{}
+}
+
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	cmdBatch := make([]tea.Cmd, 0, 1)
+	switch msg.(type) {
+	case FailedConfirmationMsg:
+		m.createNewForms()
+		cmdBatch = append(cmdBatch, m.inputForm.Init(), m.confirmForm.Init())
+	}
+	if m.inputForm.State != huh.StateCompleted {
+		inputForm, cmd := m.inputForm.Update(msg)
+		cmdBatch = append(cmdBatch, cmd)
+		if f, ok := inputForm.(*huh.Form); ok {
+			m.inputForm = f
+		}
+	} else if m.confirmForm.State != huh.StateCompleted {
+		confirmForm, cmd := m.confirmForm.Update(msg)
+		cmdBatch = append(cmdBatch, cmd)
+		if f, ok := confirmForm.(*huh.Form); ok {
+			m.confirmForm = f
+		}
+		if m.confirmForm.State == huh.StateCompleted {
+			if !m.confirmation {
+				cmdBatch = append(cmdBatch, FailedConfirmation)
+			} else {
+				cmdBatch = append(cmdBatch, tea.Quit)
+			}
+		}
+	}
+	return m, tea.Batch(cmdBatch...)
+}
+
+func (m *model) View() string {
+	if m.inputForm.State == huh.StateCompleted {
+		md := m.Markdown()
+		renderedMd, err := glamour.Render(md, "notty")
+		if err != nil {
+			renderedMd = md
+		}
+		return lipgloss.JoinVertical(
+			lipgloss.Left,
+			renderedMd,
+			m.confirmForm.View(),
+		)
+	}
+	return m.inputForm.View()
+}
